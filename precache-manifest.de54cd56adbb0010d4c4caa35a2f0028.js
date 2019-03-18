75212f4f9549161aef20afa244ec388bc4e071a5
John Pena <pemu775@gmail.com>
2019-03-18 09:24:53 -0400

    Updates

---
+++ precache-manifest.de54cd56adbb0010d4c4caa35a2f0028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "412ad3a55aea76f55ead6c10156483f4",
+    "url": "/react-city-compare/static/media/downtown.412ad3a5.png"
+  },
+  {
+    "revision": "2c2db92e319bc4a98208",
+    "url": "/react-city-compare/static/js/runtime~main.2c2db92e.js"
+  },
+  {
+    "revision": "b2501be5e3a4d9dd3215",
+    "url": "/react-city-compare/static/js/main.b2501be5.chunk.js"
+  },
+  {
+    "revision": "8cc502a10bb9af861862",
+    "url": "/react-city-compare/static/js/2.8cc502a1.chunk.js"
+  },
+  {
+    "revision": "b2501be5e3a4d9dd3215",
+    "url": "/react-city-compare/static/css/main.a15ccb8b.chunk.css"
+  },
+  {
+    "revision": "50d26ea2cd20f7993f714c85be602d9a",
+    "url": "/react-city-compare/index.html"
+  }
+];
