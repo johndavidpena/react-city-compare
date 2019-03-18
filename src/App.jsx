8b8c14cd71c1f17ac718a6775cda209a42632006
John Pena <pemu775@gmail.com>
2019-03-18 19:00:16 -0400

    Updated bg image

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import City from './containers/City/City';
 
 const App = props => (
   <div className="App">
-    {/* <header className="App__header">
-      <h1 className="App__title">Compare Cities</h1>
-    </header> */}
+    <header className="App__header">
+      <h1 className="App__title-city">City</h1>
+      <h1 className="App__title-compare">Compare</h1>
+    </header>
     <main className="App__cityWrapper">
       <City key="left" />
       <City key="right" />
